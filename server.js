19a6e731a6b0570b7b592b188a832a935965717f
System <none>
2017-08-03 20:21:09 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
+app.route('/new/:url')
+    .get(function(req, res) {
+		  res.send(req.params.url)
+    })
+
+
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
   res.status(404);

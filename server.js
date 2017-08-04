95c744c93f2ecfc2943c658fd50b2b8f4b5e5633
System <none>
2017-08-04 00:02:10 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOLAB_URI;
 var validUrl = require('valid-url');
 var urlEncoder = require('./url-encoder');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create the counters schema with an _id field and a seq field
+var CounterSchema = Schema({
+    _id: {type: String, required: true},
+    seq: { type: Number, default: 0 }
+});
+
+// create a model from that schema
+var counter = mongoose.model('counter', CounterSchema);
+
+// create a schema for our links
+var urlSchema = new Schema({
+  _id: {type: Number, index: true},
+  long_url: String,
+  created_at: Date
+});
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
